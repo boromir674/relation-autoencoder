7c1b3be10425590235abafdbff0083ae7416a9e8
Konstantinos <k.lampridis@hotmail.com>
2017-06-15 12:44:01 +0200

    populate settings paths

--- definitions/settings.py
+++ definitions/settings.py
@@ -1,10 +1,20 @@
-__author__ = 'diego'
+import os
 
-models_path = ''
-clusters_path = ''
+# root_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = '/Data/thesis/code/relation-autoencoder/'
+
+models_path = root_dir + '/train_products/'
+clusters_path = root_dir + '/train_products/'
+posteriors_path = root_dir + '/train_products/'
+retreival_metrics = root_dir + '/train_products/'
+plots_path = '/Data/thesis/plots/'
+
+# directory in which pickled files are located for unit testing purposes
+test_dir = '/Data/thesis/data/'
 
 lda_pairs_path = ''
 relations2IdDictionary = ''
+
 
 external_embeddings_path = ''
 debug = True
@@ -14,3 +24,4 @@ elems_to_visualize = 5
 low = -1.e-3
 high = 1.e-3
 
+split_labels = ['train', 'valid', 'test']
